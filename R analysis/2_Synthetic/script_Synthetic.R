1ff6bb8402992134b4cd45714126712c6db7ceb8
Jiří Novák <32874621+Kyoshido@users.noreply.github.com>
2024-09-01 18:25:41 +0200

    Update script_Synthetic.R

--- R analysis/2_Synthetic/script_Synthetic.R
+++ R analysis/2_Synthetic/script_Synthetic.R
@@ -16,39 +16,161 @@ library(ggplot2)
 
 # Dataset ---------------------------------------------------------------------
 
-# Microdata 
-df <- readRDS(
+file <- "data.csv"
+df <- read.csv(
   paste0(
     c(unlist(strsplit(getwd(), "/"))[1:(length(unlist(strsplit(getwd(), "/")))
-                                        -2)], # removing 2 levels in files hierarchy
-      "0_Data"
+                                        -1)], # removing n levels in files hierarchy
+      "0_Data",
+      file
       ),collapse = '/'
   )
+  ,sep = "\t"
 )
 
+# Variables -------------------------------------------------------------------
 
+user_answer <- c(
+  "Q1A", # "Never tell anyone the real reason you did something unless it is useful to do so.",
+  "Q2A", # "The best way to handle people is to tell them what they want to hear.",
+  "Q3A", # "One should take action only when sure it is morally right.",
+  "Q4A", # "Most people are basically good and kind.",
+  "Q5A", # "It is safest to assume that all people have a vicious streak and it will come out when they are given a chance.",
+  "Q6A", # "Honesty is the best policy in all cases.",
+  "Q7A", # "There is no excuse for lying to someone else.",
+  "Q8A", # "Generally speaking, people won't work hard unless they're forced to do so.",
+  "Q9A", # "All in all, it is better to be humble and honest than to be important and dishonest.",
+  "Q10A", # "When you ask someone to do something for you, it is best to give the real reasons for wanting it rather than giving reasons which carry more weight.",
+  "Q11A", # "Most people who get ahead in the world lead clean, moral lives.",
+  "Q12A", # "Anyone who completely trusts anyone else is asking for trouble.",
+  "Q13A", # "The biggest difference between most criminals and other people is that the criminals are stupid enough to get caught.",
+  "Q14A", # "Most people are brave.",
+  "Q15A", # "It is wise to flatter important people.",
+  "Q16A", # "It is possible to be good in all respects.",
+  "Q17A", # "P.T. Barnum was wrong when he said that there's a sucker born every minute.",
+  "Q18A", # "It is hard to get ahead without cutting corners here and there.",
+  "Q19A", # "People suffering from incurable diseases should have the choice of being put painlessly to death.",
+  "Q20A"  # "Most people forget more easily the death of their parents than the loss of their property."
+)
 
-data <- read.csv("data/data.csv",
-                 sep = "\t"
-                 )
-# str(data)
+item_position <- c(
+  "Q1I", # "Never tell anyone the real reason you did something unless it is useful to do so.",
+  "Q2I", # "The best way to handle people is to tell them what they want to hear.",
+  "Q3I", # "One should take action only when sure it is morally right.",
+  "Q4I", # "Most people are basically good and kind.",
+  "Q5I", # "It is safest to assume that all people have a vicious streak and it will come out when they are given a chance.",
+  "Q6I", # "Honesty is the best policy in all cases.",
+  "Q7I", # "There is no excuse for lying to someone else.",
+  "Q8I", # "Generally speaking, people won't work hard unless they're forced to do so.",
+  "Q9I", # "All in all, it is better to be humble and honest than to be important and dishonest.",
+  "Q10I", # "When you ask someone to do something for you, it is best to give the real reasons for wanting it rather than giving reasons which carry more weight.",
+  "Q11I", # "Most people who get ahead in the world lead clean, moral lives.",
+  "Q12I", # "Anyone who completely trusts anyone else is asking for trouble.",
+  "Q13I", # "The biggest difference between most criminals and other people is that the criminals are stupid enough to get caught.",
+  "Q14I", # "Most people are brave.",
+  "Q15I", # "It is wise to flatter important people.",
+  "Q16I", # "It is possible to be good in all respects.",
+  "Q17I", # "P.T. Barnum was wrong when he said that there's a sucker born every minute.",
+  "Q18I", # "It is hard to get ahead without cutting corners here and there.",
+  "Q19I", # "People suffering from incurable diseases should have the choice of being put painlessly to death.",
+  "Q20I"  # "Most people forget more easily the death of their parents than the loss of their property."
+)
 
-var_all <- c(
-    "age" 
-   ,"gender"  
-   ,"married"   
-   ,"religion"
-   ,"education" 
-   ,"urban"  
-   ,"engnat"  
-   ,"hand"   
-   ,"orientation" 
-   ,"race"     
-   ,"voted"       
-   ,"familysize" 
-             )
+time_question <- c(
+  "Q1E", # "Never tell anyone the real reason you did something unless it is useful to do so.",
+  "Q2E", # "The best way to handle people is to tell them what they want to hear.",
+  "Q3E", # "One should take action only when sure it is morally right.",
+  "Q4E", # "Most people are basically good and kind.",
+  "Q5E", # "It is safest to assume that all people have a vicious streak and it will come out when they are given a chance.",
+  "Q6E", # "Honesty is the best policy in all cases.",
+  "Q7E", # "There is no excuse for lying to someone else.",
+  "Q8E", # "Generally speaking, people won't work hard unless they're forced to do so.",
+  "Q9E", # "All in all, it is better to be humble and honest than to be important and dishonest.",
+  "Q10E", # "When you ask someone to do something for you, it is best to give the real reasons for wanting it rather than giving reasons which carry more weight.",
+  "Q11E", # "Most people who get ahead in the world lead clean, moral lives.",
+  "Q12E", # "Anyone who completely trusts anyone else is asking for trouble.",
+  "Q13E", # "The biggest difference between most criminals and other people is that the criminals are stupid enough to get caught.",
+  "Q14E", # "Most people are brave.",
+  "Q15E", # "It is wise to flatter important people.",
+  "Q16E", # "It is possible to be good in all respects.",
+  "Q17E", # "P.T. Barnum was wrong when he said that there's a sucker born every minute.",
+  "Q18E", # "It is hard to get ahead without cutting corners here and there.",
+  "Q19E", # "People suffering from incurable diseases should have the choice of being put painlessly to death.",
+  "Q20I"  # "Most people forget more easily the death of their parents than the loss of their property."
+)
 
-df <- data[,var_all]
+Ten_Item_Personality <- c(
+  "TIPI1", #	Extraverted, enthusiastic.
+  "TIPI2", #	Critical, quarrelsome.
+  "TIPI3", #	Dependable, self-disciplined.
+  "TIPI4", #	Anxious, easily upset.
+  "TIPI5", #	Open to new experiences, complex.
+  "TIPI6", #	Reserved, quiet.
+  "TIPI7", #	Sympathetic, warm.
+  "TIPI8", #	Disorganized, careless.
+  "TIPI9", #	Calm, emotionally stable.
+  "TIPI10" #	Conventional, uncreative.
+)
+
+check_list <- c(
+  "VCL1", #	boat
+  "VCL2", #	incoherent
+  "VCL3", #	pallid
+  "VCL4", #	robot
+  "VCL5", #	audible
+  "VCL6", #	cuivocal
+  "VCL7", #	paucity
+  "VCL8", #	epistemology
+  "VCL9", #	florted
+  "VCL10", #	decide
+  "VCL11", #	pastiche
+  "VCL12", #	verdid
+  "VCL13", #	abysmal
+  "VCL14", #	lucid
+  "VCL15", #	betray
+  "VCL16"  #	funny
+  
+)
+
+demographics <- c(
+  "education", # "How much education have you completed?", 
+  #1=Less than high school, 2=High school, 3=University degree, 4=Graduate degree
+  "urban", #				"What type of area did you live when you were a child?", 
+  #1=Rural (country side), 2=Suburban, 3=Urban (town, city)
+  "gender", #				"What is your gender?", 
+  #1=Male, 2=Female, 3=Other
+  "engnat", #				"Is English your native language?", 
+  #1=Yes, 2=No
+  "age", #					"How many years old are you?"
+  "hand", #				"What hand do you use to write with?", 
+  #1=Right, 2=Left, 3=Both
+  "religion", #			"What is your religion?", 
+  #1=Agnostic, 2=Atheist, 3=Buddhist, 4=Christian (Catholic), 5=Christian (Mormon), 6=Christian (Protestant), 7=Christian (Other), 8=Hindu, 9=Jewish, 10=Muslim, 11=Sikh, 12=Other
+  "orientation", #			"What is your sexual orientation?", 
+  #1=Heterosexual, 2=Bisexual, 3=Homosexual, 4=Asexual, 5=Other
+  "race", #				"What is your race?", 
+  #10=Asian, 20=Arab, 30=Black, 40=Indigenous Australian, 50=Native American, 60=White, 70=Other
+  "voted", #				"Have you voted in a national election in the past year?", 
+  #1=Yes, 2=No
+  "married", #				"What is your marital status?", 
+  #1=Never married, 2=Currently married, 3=Previously married
+  "familysize", #			"Including you, how many children did your mother have?"		
+  "major" #				"If you attended a university, what was your major (e.g. "psychology", "English", "civil engineering")?"
+)
+
+server <- c(
+  "country", # the user's network location
+  "screenw", # width of user's device in pixels
+  "screenh"	 # width of user's device in pixels
+)
+
+time_page <- c(
+  "introelapse",
+  "testelapse",
+  "surveyelapse"
+) 
+
+data <- df[,c(-user_answer)]
 
 # "age" 
 table(df$age)
